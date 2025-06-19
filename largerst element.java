636969a4f0555a9cb9bbaa2f92f740488ea62235
ramyagovindharaj <155719078+ramyagovindharaj@users.noreply.github.com>
2025-06-19 16:28:37 +0530

    Update largerst element.java

--- largerst element.java
+++ largerst element.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int[] arr = {10, 50, 30, 20, 40};
-        java.util.Arrays.sort(arr);  // Sort the array
+        java.util.Arrays.sort(arr);  
 
         int secondLargest = arr[arr.length - 2];  
         System.out.println("Second Largest: " + secondLargest);
